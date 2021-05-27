836b28057e73921d0a661d7a8815b4bf98f7d75e
Vishal Yadav <vishal.yadav@neosoftmail.com>
2021-05-27 20:21:27 +0530

    eventbinding

--- src/Pages/Register/register.js
+++ src/Pages/Register/register.js
@@ -6,13 +6,85 @@ import { Link } from "react-router-dom";
 
 function Register() {
   const [isregistered, setisregistered] = useState(true);
+  const [loginValues, setLoginValues] = useState({
+    email: "",
+    password: "",
+    rememberme: ""
+  })
+  const [registerValues, setRegisterValues] = useState({
+    email: "",
+    password: "",
+    confirmpassword: ""
+  })
 
+  const handleLogin = (e) => {
+    const value = e.target.value;
+    setLoginValues({
+      ...loginValues,
+      [e.target.name]: value,
+    })
+  }
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault()
+    alert("Logged In")
+  }
+
+  const handleRegister = (e) => {
+    const value = e.target.value;
+    setRegisterValues({
+      ...registerValues,
+      [e.target.name]: value,
+    })
+  }
+  const handleRegisterSubmit = (e) => {
+    e.preventDefault()
+    alert("Registered!!")
+  }
+
+  const checkboxFunc = (e) => {
+    console.log(e)
+  }
   return (
     <Container fluid className="wrapper">
       <Row>
         <Col md={6}>hello</Col>
         <Col md={6} className="colwrapper">
           {isregistered ? (
+            <div className="loginformwarapper">
+              <div className="icon">
+                <img
+                  src={lock}
+                  alt="lock"
+                  style={{ height: "100px", width: "100px" }}
+                />
+              </div>
+              <hr />
+              <Form onSubmit={handleLoginSubmit}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control name="email" value={loginValues.email} type="email" placeholder="Enter email" onChange={handleLogin} />
+                </Form.Group>
+                <br></br>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control name="password" value={loginValues.password} type="password" placeholder="Password" onChange={handleLogin} />
+                </Form.Group>
+                <br></br>
+                <Form.Group controlId="formBasicCheckbox">
+                  <Form.Check name="rememberme" type="checkbox" label="Remember Me !!" onChange={handleLogin}/>
+                </Form.Group>
+                <br></br>
+                <hr />
+                <div className="footer">
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                  <Link onClick={() => setisregistered(!isregistered)}>register here !!</Link>
+                </div>
+              </Form>
+            </div>
+          ) : (
             <div className="formwrapper">
               <div className="icon">
                 <img
@@ -22,69 +94,35 @@ function Register() {
                 />
               </div>
               <hr />
-              <Form>
+              
+              <Form onSubmit={handleRegisterSubmit}>
                 <Form.Group controlId="formBasicEmail">
                   <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Control name="email" value={registerValues.email} type="email" placeholder="Enter email" onChange={handleRegister} />
                 </Form.Group>
-                <br></br>
+                <br />
                 <Form.Group controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control name="password" value={registerValues.password} type="password" placeholder="Password" onChange={handleRegister} />
                 </Form.Group>
-                <br></br>
-                <Form.Group controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Remeber Me !!" />
+                <br />
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control name="confirmpassword" value={registerValues.confirmpassword} type="password" placeholder="Confirm Password" onChange={handleRegister} />
                 </Form.Group>
-                <br></br>
-                <hr/>
+                <br />
+                <Form.Group >
+                    <Form.File placeholder="Upload Image" />
+                </Form.Group>
+                <hr />
                 <div className="footer">
                   <Button variant="primary" type="submit">
                     Submit
                   </Button>
-                  <Link onClick={()=>setisregistered(!isregistered)}>register here !!</Link>
+                  <Link onClick={() => setisregistered(!isregistered)}>already register !!</Link>
                 </div>
               </Form>
             </div>
-          ) : (
-            <div className="formwrapper">
-            <div className="icon">
-              <img
-                src={lock}
-                alt="lock"
-                style={{ height: "100px", width: "100px" }}
-              />
-            </div>
-            <hr />
-            <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
-              <br></br>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <br></br>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <br></br>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Co</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <hr/>
-              <div className="footer">
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>
-                <Link onClick={()=>setisregistered(!isregistered)}>already register !!</Link>
-              </div>
-            </Form>
-          </div>
           )}
         </Col>
       </Row>
