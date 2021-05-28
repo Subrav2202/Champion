6a2cf9ce0f139e0e84ba31907b0f7dbabe7b80ab
Vishal Yadav <vishal.yadav@neosoftmail.com>
2021-05-28 18:07:47 +0530

    eyeIcons

--- src/Pages/constant.js
+++ src/Pages/constant.js
@@ -1,5 +1,11 @@
-export const margintop=()=>{
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+
+export const eye = <FontAwesomeIcon icon={faEye} />;
+export const eyeSlash = <FontAwesomeIcon icon={faEyeSlash} />;
+
+export const margintop = () => {
     let value=Math.floor(Math.random() * (150- 20 + 1)) + 20;
     return value;
 }
-
