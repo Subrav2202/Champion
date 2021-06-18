6d3068d2dbad7c382ebcdfb2b7e8fd72d4baf997
Vishal Yadav <vishal.yadav@neosoftmail.com>
2021-06-18 12:16:40 +0530

    demo commit

--- src/Pages/Components/Dashboard/chartsData.js
+++ src/Pages/Components/Dashboard/chartsData.js
@@ -1,92 +1,93 @@
 export const chartsarray = [
-  {
-    month: "January",
-    weeks: [
-      {
-        date: "1/01",
-        vote: 10,
-      },
-      {
-        date: "8/01",
-        vote: 15,
-      },
-      {
-        date: "15/01",
-        vote: 2,
-      },
-      {
-        date: "22/01",
-        vote: 4,
-      },
-      {
-        date: "29/01",
-        vote: 10,
-      },
-    ],
-  },
-  {
-    month: "Febuary",
-    weeks: [
-      {
-        date: "5/01",
-        vote: 2,
-      },
-      {
-        date: "13/01",
-        vote: 15,
-      },
-      {
-        date: "21/01",
-        vote: 1,
-      },
-      {
-        date: "28/01",
-        vote: 4,
-      },
-      {
-        date: "12/01",
-        vote: 4,
-      },
-    ],
-  },
-  {
-    month: "June",
-    weeks: [
-      {
-        date: "5/01",
-        vote: 2,
-      },
-      {
-        date: "13/01",
-        vote: 4,
-      },
-      {
-        date: "21/01",
-        vote: 17,
-      },
-      {
-        date: "28/01",
-        vote: 20,
-      },
-    ],
-  },
-];
-
-export const recentVotes = [
-  {
-    username: "vishal",
-    votes: 5,
-  },
-  {
-    username: "Subrao",
-    votes: 8,
-  },
-  {
-    username: "Abhishek",
-    votes: 3,
-  },
-  {
-    username: "Nishant",
-    votes: 5,
-  },
-];
+    {
+      month: "January",
+      weeks: [
+        {
+          date: "1/01",
+          vote: 10,
+        },
+        {
+          date: "8/01",
+          vote: 15,
+        },
+        {
+          date: "15/01",
+          vote: 2,
+        },
+        {
+          date: "22/01",
+          vote: 4,
+        },
+        {
+          date: "29/01",
+          vote: 10,
+        },
+      ],
+    },
+    {
+      month: "Febuary",
+      weeks: [
+        {
+          date: "5/01",
+          vote: 2,
+        },
+        {
+          date: "13/01",
+          vote: 15,
+        },
+        {
+          date: "21/01",
+          vote: 1,
+        },
+        {
+          date: "28/01",
+          vote: 4,
+        },
+        {
+          date: "12/01",
+          vote: 4,
+        },
+      ],
+    },
+    {
+      month: "June",
+      weeks: [
+        {
+          date: "5/01",
+          vote: 2,
+        },
+        {
+          date: "13/01",
+          vote: 4,
+        },
+        {
+          date: "21/01",
+          vote: 17,
+        },
+        {
+          date: "28/01",
+          vote: 20,
+        },
+      ],
+    },
+  ];
+  
+  export const recentVotes = [
+    {
+      username: "vishal",
+      votes: 5,
+    },
+    {
+      username: "Subrao",
+      votes: 8,
+    },
+    {
+      username: "Abhishek",
+      votes: 3,
+    },
+    {
+      username: "Nishant",
+      votes: 5,
+    },
+  ];
+  
